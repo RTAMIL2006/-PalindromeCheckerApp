43d037c392aec2a948207d8b4afc042ecb0e44d9
RTAMIL2006 <tamilselvan2872006@gmail.com>
2026-02-20 10:20:22 +0530

    UC4:array based Palindrome Check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -15,20 +15,33 @@ public class PalindromeCheckerApp {
         System.out.println("Application Started Successfully!");
         System.out.println("---------------------------------");
 
-        String word = "madam";
-        String reversed = "";
+        String word = "radar";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        // Convert string to character array
+        char[] characters = word.toCharArray();
+
+        // Two-pointer approach
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
 
-        if (word.equals(reversed)) {
+        // Display result
+        if (isPalindrome) {
             System.out.println("The given string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The given string \"" + word + "\" is NOT a Palindrome.");
-
-
-
         }
     }
 }
